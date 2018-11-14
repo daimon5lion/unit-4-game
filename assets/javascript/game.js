ee4e09926f3558dcf7f9b5648378f5e51c8c6295
Lei <leichen83@gmail.com>
2018-11-14 15:53:11 -0500

    worked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var yourScore = 0;
   var wins = 0;
-  var looses = 0;
+  var loses = 0;
 
   var tNumbers = Math.floor(Math.random() * 102 + 19);
   $("#targetnumber").text(tNumbers);
@@ -11,7 +11,57 @@ $(document).ready(function() {
   var c3Numbers = Math.floor(Math.random() * 12 + 1);
   var c4Numbers = Math.floor(Math.random() * 12 + 1);
 
-  function rest() {
+  function win() {
+    alert("Congrats! You won!");
+    wins++;
+    $("#win").text(wins);
+    reset();
+  }
+  function loss() {
+    alert("Sorry! You lose!");
+    loses++;
+    $("#lose").text(loses);
+    reset();
+  }
+
+  $("#cr1").on("click", function() {
+    yourScore = yourScore + c1Numbers;
+    $("#score").text(yourScore);
+    if (yourScore == tNumbers) {
+      win();
+    } else if (yourScore > tNumbers) {
+      loss();
+    }
+  });
+  $("#cr2").on("click", function() {
+    yourScore = yourScore + c2Numbers;
+    $("#score").text(yourScore);
+    if (yourScore == tNumbers) {
+      win();
+    } else if (yourScore > tNumbers) {
+      loss();
+    }
+  });
+  $("#cr3").on("click", function() {
+    yourScore = yourScore + c3Numbers;
+    $("#score").text(yourScore);
+    if (yourScore == tNumbers) {
+      win();
+    } else if (yourScore > tNumbers) {
+      loss();
+    }
+  });
+  $("#cr4").on("click", function() {
+    yourScore = yourScore + c4Numbers;
+    $("#score").text(yourScore);
+    if (yourScore == tNumbers) {
+      win();
+    } else if (yourScore > tNumbers) {
+      loss();
+    }
+  });
+
+  function reset() {
     tNumbers = Math.floor(Math.random() * 102 + 19);
     $("#targetnumber").text(tNumbers);
     c1Numbers = Math.floor(Math.random() * 12 + 1);
@@ -19,22 +69,6 @@ $(document).ready(function() {
     c3Numbers = Math.floor(Math.random() * 12 + 1);
     c4Numbers = Math.floor(Math.random() * 12 + 1);
     yourScore = 0;
+    $("#score").text(yourScore);
   }
-
-  $("#cr1").on("click", function() {
-    yourScore = yourScore + c1Numbers;
-    $("#score").text(yourScore);
-  });
-  $("#cr2").on("click", function() {
-    yourScore = yourScore + c2Numbers;
-    $("#score").text(yourScore);
-  });
-  $("#cr3").on("click", function() {
-    yourScore = yourScore + c3Numbers;
-    $("#score").text(yourScore);
-  });
-  $("#cr4").on("click", function() {
-    yourScore = yourScore + c4Numbers;
-    $("#score").text(yourScore);
-  });
 });
